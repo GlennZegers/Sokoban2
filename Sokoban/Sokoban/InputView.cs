d3cc3a8a9924ad7956ea3e35edc14c071c2a454c
annadame <ajdame@avans.nl>
2018-09-18 14:44:06 +0200

    begin bericht

--- Sokoban/Sokoban/InputView.cs
+++ Sokoban/Sokoban/InputView.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Sokoban
 {
     public class InputView
     {
+        public int ChooseMaze()
+        {
+            bool isValid = false;
+            int MazeNumber = 0;
+
+            while (!isValid)
+            {
+                Console.Write("> Kies een doolhof (1 - 4), s = stop");
+                Console.WriteLine();
+                string input = Console.ReadLine();
+
+                if (input.Equals("s"))
+                {
+                    isValid = true;
+                    Environment.Exit(0);
+                    return 0;
+                }
+
+                if (Regex.IsMatch(input, @"^\d+$") && Int32.Parse(input) >= 1 && Int32.Parse(input) <= 4)
+                {
+                    MazeNumber = Int32.Parse(input);
+                    isValid = true;
+                }
+            }
+            return MazeNumber;
+        }
     }
 }
--- Sokoban/Sokoban/OutputView.cs
+++ Sokoban/Sokoban/OutputView.cs
@@ -7,5 +7,34 @@ namespace Sokoban
 {
     public class OutputView
     {
+        public void StartMessage()
+        {
+            Console.Write("┌────────────────────────────────────────────────────┐");
+            Console.WriteLine();
+            Console.Write("| Welkom bij Sokoban                                 |");
+            Console.WriteLine();
+            Console.Write("|                                                    |");
+            Console.WriteLine();
+            Console.Write("| betekenis van de symbolen   |   doel van het spel  |");
+            Console.WriteLine();
+            Console.Write("|                             |                      |");
+            Console.WriteLine();
+            Console.Write("| spatie : outerspace         |   duw met de truck   |");
+            Console.WriteLine();
+            Console.Write("|      █ : muur               |   de krat(ten)       |");
+            Console.WriteLine();
+            Console.Write("|      · : vloer              |   naar de bestemming |");
+            Console.WriteLine();
+            Console.Write("|      O : krat               |                      |");
+            Console.WriteLine();
+            Console.Write("|      0 : krat op bestemming |                      |");
+            Console.WriteLine();
+            Console.Write("|      x : bestemming         |                      |");
+            Console.WriteLine();
+            Console.Write("|      @ : truck              |                      |");
+            Console.WriteLine();
+            Console.Write("└────────────────────────────────────────────────────┘");
+            Console.WriteLine();
+        }
     }
 }
