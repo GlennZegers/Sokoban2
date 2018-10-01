38e7e7e5b5f4f6e11a12197a4d92dee746b6bb79
Glenn Zegers <gtmzeger@avans.nl>
2018-10-01 10:49:13 +0200

    Moveable

--- Sokoban/Sokoban/Model/Crate.cs
+++ Sokoban/Sokoban/Model/Crate.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Sokoban.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Sokoban
 {
-    public class Crate
+    public class Crate : Moveable
     {
         public Field CurrentField { get; set; }
 
